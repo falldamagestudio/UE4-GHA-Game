01d33ee622a66eb7a8ccfabd3b3f693784737d9f
Mikael Kalms <mikael@kalms.org>
2020-06-01 13:46:06 +0200

    Run-Editor and Run-UAT throw at nonzero exit codes

--- Scripts/Windows/Run-Editor.ps1
+++ Scripts/Windows/Run-Editor.ps1
@@ -1,6 +1,12 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 . "$here\Get-EngineLocationForProject.ps1"
 
+class UE4EditorException : Exception {
+	$ExitCode
+
+	UE4EditorException([int] $exitCode) : base("UE4Editor exited with code ${exitCode}") { $this.ExitCode = $exitCode }
+}
+
 function Run-Editor {
 
 	param (
@@ -14,5 +20,10 @@ function Run-Editor {
 	
 	$EditorArguments = @($UProjectLocation) + $Arguments | Where { $_ -ne $null }
 
-	Start-Process -FilePath $EditorLocation -ArgumentList $EditorArguments -NoNewWindow -Wait
+	$Process = Start-Process -FilePath $EditorLocation -ArgumentList $EditorArguments -NoNewWindow -Wait -PassThru
+
+	if ($Process.ExitCode -ne 0) {
+		throw [UE4EditorException]::new($Process.ExitCode)
+	}
 }
+
--- Scripts/Windows/Run-UAT.ps1
+++ Scripts/Windows/Run-UAT.ps1
@@ -1,6 +1,12 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 . "$here\Get-EngineLocationForProject.ps1"
 
+class UATException : Exception {
+	$ExitCode
+
+	UATException([int] $exitCode) : base("Run-UAT exited with code ${exitCode}") { $this.ExitCode = $exitCode }
+}
+
 function Run-UAT {
 
 	param (
@@ -14,5 +20,9 @@ function Run-UAT {
 	
 	$RunUATArguments = $Arguments | Where { $_ -ne $null }
 
-	Start-Process -FilePath $RunUATLocation -ArgumentList $RunUATArguments -NoNewWindow -Wait
+	$Process = Start-Process -FilePath $RunUATLocation -ArgumentList $RunUATArguments -NoNewWindow -Wait -PassThru
+	
+	if ($Process.ExitCode -ne 0) {
+		throw [UATException]::new($Process.ExitCode)
+	}
 }
