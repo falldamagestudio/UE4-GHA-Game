182109378a86ac75ae2512aeed483678ae3e8304
Mikael Kalms <mikael@kalms.org>
2020-07-07 01:40:28 +0200

    Enable iterative cooking + iterative deploy

--- BuildGame.ps1
+++ BuildGame.ps1
@@ -11,5 +11,5 @@ function BuildGame {
 		[Parameter(Mandatory)] [string] $ArchiveDir
 	)
 
-	Run-UAT -UProjectLocation $ProjectLocation -Arguments "-ScriptsForProject=$(Resolve-Path $ProjectLocation)", "BuildCookRun", "-installed", "-nop4", "-project=$(Resolve-Path $ProjectLocation)", "-cook", "-stage", "-archive", "-archivedirectory=$(Resolve-Path $ArchiveDir)", "-package", "-pak", "-prereqs", "-nodebuginfo", "-targetplatform=$TargetPlatform", "-build", "-target=$Target", "-clientconfig=$Configuration", "-utf8output", "-buildmachine"
+	Run-UAT -UProjectLocation $ProjectLocation -Arguments "-ScriptsForProject=$(Resolve-Path $ProjectLocation)", "BuildCookRun", "-installed", "-nop4", "-project=$(Resolve-Path $ProjectLocation)", "-cook", "-stage", "-archive", "-archivedirectory=$(Resolve-Path $ArchiveDir)", "-package", "-pak", "-prereqs", "-nodebuginfo", "-targetplatform=$TargetPlatform", "-build", "-target=$Target", "-clientconfig=$Configuration", "-utf8output", "-buildmachine", "-iterativecooking", "-iterativedeploy"
 }
